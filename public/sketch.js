2499c297da5cadbe27f7e55edba4260696bfee19
Benedikt Groß <bg@benedikt-gross.de>
2020-04-16 15:26:37 +0200

    Update sketch.js

--- public/sketch.js
+++ public/sketch.js
@@ -1,9 +1,9 @@
 let socket;
 
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background('#eeeeee');
+  cursor(CROSS);
 
   socket = io.connect('http://' + window.location.hostname);
   socket.on('mouse', drawOther);
@@ -11,23 +11,30 @@ function setup() {
 
 function mouseDragged() {
   const data = {
-    x: mouseX,
-    y: mouseY
+    mouseX: mouseX,
+    mouseY: mouseY,
+    pmouseX: pmouseX,
+    pmouseY: pmouseY,
   };
   // console.log('Sending: ' + mouseX + ',' + mouseY);
   socket.emit('mouse', data);
 
   // draw my mouse
-  noStroke();
-  fill(255);
-  ellipse(mouseX, mouseY, 3, 3);
+  drawLine(mouseX, mouseY, pmouseX, pmouseY, 0);
 }
 
 function drawOther(data) {
   // console.log('Received: ', data);
-  noStroke();
-  fill(255, 0, 100);
-  ellipse(data.x, data.y, 3, 3);
+  drawLine(data.mouseX, data.mouseY, data.pmouseX, data.pmouseY, 0);
+}
+
+function drawLine(x1, y1, x2, y2, col) {
+  let dx = x2 - x1;
+  let dy = y2 - y1;
+  let d = Math.sqrt(dx * dx + dy * dy) * 0.015;
+  strokeWeight(0.5);
+  stroke(col, (0.7 - d) * 255);
+  line(x1, y1, x2, y2);
 }
 
 function draw() {}
