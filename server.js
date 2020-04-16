7c7caa175caecf36c03a8133fcc0ec991a63feed
Benedikt Groß <bg@benedikt-gross.de>
2020-04-16 15:07:50 +0200

    Clean up server.js

--- server.js
+++ server.js
@@ -2,25 +2,20 @@ const express = require('express');
 const socket = require('socket.io');
 
 const app = express();
-const server = app.listen(process.env.PORT || 3000, function() {
+const server = app.listen(process.env.PORT || 80, () => {
   console.log(`Listening on port: ${server.address().port}`);
 });
 const io = socket(server);
 
 app.use(express.static('public'));
 
-io.sockets.on('connection', newConnection);
-
-function newConnection(socket) {
+io.sockets.on('connection', socket => {
   console.log('new connection: ' + socket.id);
+  socket.on('mouse', data => {
+    // broadcast to all clients including initial sender
+    // io.sockets.emit('mouse', data)
 
-  socket.on('mouse', mouseMsg);
-
-  function mouseMsg(data) {
-    //Broadcast to all clients
-    //io.sockets.emit('mouse',data)
-    //client broadcasts to other clients
-    socket.broadcast.emit('mouse', data); //send the same message back out
-    // console.log(data);
-  }
-}
+    // client broadcasts to other clients excluding sender
+    socket.broadcast.emit('mouse', data);
+  });
+});
